e7e0d754d4f3dacd4cda3ddb0ba4e744a8971234
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-17 13:18:50 -0400

    Update app.py

--- the-nest-exchange/src/Backend/app.py
+++ the-nest-exchange/src/Backend/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from routes import auth
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'nestexchange'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Team23_2@database-1.cjm0e6m6u6vm.us-east-2.rds.amazonaws.com:5432/postgres'
 db = SQLAlchemy(app)
 
 app.register_blueprint(auth)
