d8d34c722092ba2ef72fe1d9ba77b2c505892727
Jared Sapp <jaredbsapp@gmail.com>
2025-03-18 15:42:53 -0400

    testConnection

--- the-nest-exchange/src/Backend/app.py
+++ the-nest-exchange/src/Backend/app.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 from routes import auth
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql+psycopg2://{user}:{password}@{host}/{database}'
 db = SQLAlchemy(app)
 
 app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
