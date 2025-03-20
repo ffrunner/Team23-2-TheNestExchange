2b280a3af273d213cad4712a5822c0ade8a9d653
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-20 14:13:59 -0400

    Update app.py

--- the-nest-exchange/src/Backend/app.py
+++ the-nest-exchange/src/Backend/app.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 
 
+def create app():
+    app = Flask(__name__)
+    db = SQLAlchemy(app)
+    
+    app.register_blueprint(auth)
+    return app
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run(debug=True)
