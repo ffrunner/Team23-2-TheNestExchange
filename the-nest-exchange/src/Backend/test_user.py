43cac3d4aea0aa98f12aaf0a47e270194a1086cb
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-21 11:20:29 -0400

    Create test_user.py

---
+++ the-nest-exchange/src/Backend/test_user.py
@@ -0,0 +1,12 @@
+from database_connect import connect_database
+from user import create_user, delete_user
+
+if __name__ == "__main__":
+    try:
+        conn = connect_database()
+        create_user("1", "john1", "admin", "fakeemail", "john", "doe", "1111111111")
+        delete_user("1")
+        conn.close()
+    except: 
+        conn.close()
+        print("Errors")
