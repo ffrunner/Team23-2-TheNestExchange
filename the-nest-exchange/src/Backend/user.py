46c6206bf1145ada1bcfc75b290cc652e772cb3b
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-21 11:12:38 -0400

    Create user.py

---
+++ the-nest-exchange/src/Backend/user.py
@@ -0,0 +1,28 @@
+from database_connect import connect_database
+
+def create_user(id, username, password_hash, role, email, first_name, last_name, phone):
+   try:
+        conn = connect_database()
+        cursor = conn.cursor()
+        query = "INSERT INTO Users (id, username, password_hash, role, email, first_name, last_name, phone, date_created, last_login) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(query, (id, username, password_hash, role, email, first_name, last_name, phone))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return "User created successfully"
+   except Exception as e:
+       print(f"Error: {e}")
+
+
+def delete_user(id):
+    try:
+        conn = connect_database()
+        cursor = conn.cursor()
+        query = "DELETE FROM Users WHERE id = %s"
+        cursor.execute(query,(id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return "User deleted successfully"
+    except Exception as e:
+        print(f"Eroror: {e}")
