8f5fe207c26f60611cd82d7c7555c258f61e5d06
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-21 16:23:16 -0400

    Update test_user.py

--- the-nest-exchange/src/Backend/test_user.py
+++ the-nest-exchange/src/Backend/test_user.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     try:
         conn = connect_database()
         create_user("1", "john1", "adlhfadhfoadshfdoush", "admin", "fakeemail", "john", "doe", "1111111111")
-        delete_user("1")
         conn.close()
-        print("Created and deleted user successfully")
+        print("Created user successfully")
     except: 
         conn.close()
         print("Errors")
