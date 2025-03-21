46fda5617cbc4907225a593733cbcef4d6a695e3
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-03-21 12:26:46 -0400

    Update app.py

--- the-nest-exchange/src/Backend/app.py
+++ the-nest-exchange/src/Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from auth import auth_bp
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Team23_2@database-1.cjm0e6m6u6vm.us-east-2.rds.amazonaws.com:5432/postgres'
@@ -8,8 +9,8 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return "This is the landing page"
-app.register_blueprint(auth_bp)
-if__name__== "__main__":
+
+if __name__== "__main__":
     app.run(debug=True)
 
 
